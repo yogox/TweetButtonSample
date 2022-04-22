c65c1e91c107627656bd69e098afcc6ecc8003e7
yogox <yogoyogox@gmail.com>
2022-04-22 09:05:20 +0900

    Initial Commit

---
+++ TweetButtonSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TweetButtonSample
+//
+//  Created by yogox on 2022/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
