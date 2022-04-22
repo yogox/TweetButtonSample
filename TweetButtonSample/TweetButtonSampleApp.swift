021ca1dc04f79e798d230688a7ed8cbb3a7bc10f
yogox <yogoyogox@gmail.com>
2022-04-22 09:05:20 +0900

    Initial Commit

---
+++ TweetButtonSample/TweetButtonSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TweetButtonSampleApp.swift
+//  TweetButtonSample
+//
+//  Created by yogox on 2022/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TweetButtonSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
